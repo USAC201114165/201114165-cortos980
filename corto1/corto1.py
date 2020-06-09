a87d5f87e443f8028b68a3be6474f359ef522b5f
USAC201114165 <2461642630301@ingenieria.usac.edu.gt>
2020-06-09 14:00:31 -0600

    todo como debe ser

--- corto1/corto1.py
+++ corto1/corto1.py
@@ -9,4 +9,4 @@ def secuencia():
             dato = (i * 3) + 1
 imprimir = lista.append(dato)
 print ('la lista queda', str(imprimir))
-file = open('texto.txt', 'a')
+archivo = open('texto.txt', 'a')
