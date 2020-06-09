92691e9e68d383d53e2459cad06856ab30d56952
USAC201114165 <2461642630301@ingenieria.usac.edu.gt>
2020-06-09 13:04:47 -0600

    un poco tarde

--- corto1/corto1.py
+++ corto1/corto1.py
@@ -1,11 +1,12 @@
 carnet = (input('ingrese su carnet: \n'))
+dato = 0
 lista = []
 def secuencia():
     for i in range(carnet):
         if (i % 2==0):
             dato = i
-        else (i % 2!=0):
+        else:
             dato = (i * 3) + 1
 imprimir = lista.append(dato)
 print ('la lista queda', str(imprimir))
-archivo = open('texto', 'a')
+archivo = open('texto.txt', 'a')
