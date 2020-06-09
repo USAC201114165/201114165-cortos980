345d863a410a67723043199162f2c42e1b447586
USAC201114165 <2461642630301@ingenieria.usac.edu.gt>
2020-06-09 12:31:29 -0600

    prueba 1

---
+++ corto1/corto1.py
@@ -0,0 +1,11 @@
+carnet = (input('ingrese su carnet: \n'))
+lista = []
+def secuencia():
+    for i in range(carnet):
+        if (i % 2==0):
+            dato = i
+        else 
+            (i % 2!==0):
+            dato = (i / 3) + 1
+imprimir = lista.append(dato)
+print ('la lista queda', str(imprimir))
